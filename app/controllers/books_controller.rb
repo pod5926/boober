4749466c2711b28ad2fb8cfa6d9695c8fa6afcb2
pod5926 <ks.apple.5926@gmail.com>
2020-02-25 21:24:09 +0900

    Book詳細ページ追加+Userリンク作成 #9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,14 @@
 class BooksController < ApplicationController
-  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :logged_in_user, only: [:create, :destroy, :show]
   before_action :correct_user,   only: :destroy
+
+  def show
+    begin
+      @book = Book.find(params[:id])
+    rescue ActiveRecord::RecordNotFound #削除したときの遷移
+      redirect_to root_url
+    end
+  end
 
   def create
     @book = current_user.books.build(book_params)
