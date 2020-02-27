f3eb47622ffef358554b1d5cbeff61a32ca9e881
pod5926 <ks.apple.5926@gmail.com>
2020-02-27 10:54:15 +0900

    メディア対応 テストユーザー作成  #14

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,27 @@
 Faker::Config.locale = :ja
 
-User.create!(name:  "Example User",
+User.create!(
+  name:  "Example User",
   email: "example@railstutorial.org",
   password:              "foobar",
   password_confirmation: "foobar",
   admin:     true,
   activated: true,
-  activated_at: Time.zone.now)
+  activated_at: Time.zone.now
+)
 
 30.times do |n|
-name  = Faker::Name.name
-email = "example-#{n+1}@railstutorial.org"
-password = "password"
-User.create!(name:  name,
-  email: email,
-  password:              password,
-  password_confirmation: password,
-  activated: true,
-  activated_at: Time.zone.now)
+  name  = Faker::Name.name
+  email = "example-#{n+1}@gmail.com"
+  password = "password"
+  User.create!(
+    name:  name,
+    email: email,
+    password:              password,
+    password_confirmation: password,
+    activated: true,
+    activated_at: Time.zone.now
+  )
 end
 
 #本のダミー
@@ -36,3 +40,12 @@ users.each do |user|
     user.books.create!(title: title, content: content)
   end
 end
+
+User.create!(
+  name:  "test-user",
+  email: "test@gmail.com",
+  password:              "password",
+  password_confirmation: "password",
+  activated: true,
+  activated_at: Time.zone.now
+)
