4f5f4bb107a9cf7e7089b9b9a62f074cac4cc130
pod5926 <ks.apple.5926@gmail.com>
2020-02-29 15:30:23 +0900

    メールリンク修正 #13

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://bookshelf-iyefvtwh-27069184.herokuapp.com'
+  host = 'https://boober-iyefvtwh-27069184.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
